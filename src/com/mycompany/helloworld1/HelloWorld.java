ef65a857838f3cce77999cf743cdfa3a06c57d2e
Angelus <hervenougnon@hotmail.com>
2023-10-30 16:51:43 +0100

    firt version of the class

---
+++ src/com/mycompany/helloworld1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.mycompany.helloworld1;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String ... args){
+        System.out.println("Je vous salue le monde!!!!");
+    }
+}
